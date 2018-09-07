8bc2f4295e07d68ec2564fe066c039f7eadf83b0
Luis Eduardo Vela Ruiz <luiseduardovelaruiz@Luiss-MacBook-Pro.local>
2018-09-07 00:27:53 -0500

    ...

--- app/src/main/java/com/example/luiseduardovelaruiz/hopping/MenuActivity.kt
+++ app/src/main/java/com/example/luiseduardovelaruiz/hopping/MenuActivity.kt
@@ -2,25 +2,24 @@ package com.example.luiseduardovelaruiz.hopping
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Toast
+import com.example.luiseduardovelaruiz.hopping.fragments.MainMenu
+import com.example.luiseduardovelaruiz.hopping.fragments.MediaFeed
 import kotlinx.android.synthetic.main.activity_menu.*
 
 class MenuActivity : AppCompatActivity() {
+
+    var pagerAdapter: PagerAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
 
-        imageButton4.setOnClickListener(View.OnClickListener {
-            Toast.makeText(this@MenuActivity,"clicked a",Toast.LENGTH_LONG).show()
-        })
-        imageButton5.setOnClickListener(View.OnClickListener {
-            Toast.makeText(this@MenuActivity,"clicked b",Toast.LENGTH_LONG).show()
-        })
-        imageButton6.setOnClickListener(View.OnClickListener {
-            Toast.makeText(this@MenuActivity,"clicked c",Toast.LENGTH_LONG).show()
-        })
+        pagerAdapter = PagerAdapter(supportFragmentManager)
+        pagerAdapter!!.addFragmentsToAdapter(MainMenu(),"Main Menu Fragment")
+        pagerAdapter!!.addFragmentsToAdapter(MediaFeed(),"Media Feed Fragment")
+
+        myViewPager.adapter = pagerAdapter
+
 
     }//end onCreate
 }//end class MenuActivity
